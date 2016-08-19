597b5735b2ef146979bd66c50c98516996d402d4
Scott R. Santos <halocaridina@gmail.com>
2016-08-19 08:24:25 -0500

    Tweak separator

--- config/usr_local_bin/weather_report.sh
+++ config/usr_local_bin/weather_report.sh
@@ -1,7 +1,7 @@
 
 #!/bin/bash
 
-LOCWEATHER="`wunderground.py | grep -E '(Location|Condition|Temperature)' | sed 's/Location : //;s/Condition : /: /;s/Temperature ://;s/ C)/ °C)/;s/ F / °F /' | xargs`"
+LOCWEATHER="`wunderground.py | grep -E '(Location|Condition|Temperature)' | sed 's/Location : //;s/Condition : /: /;s/Temperature :/ -- /;s/ C)/ °C)/;s/ F / °F /' | xargs`"
 
 if ! [ "`ping -c 1 google.com`" ]; then
     echo ": N/A"
